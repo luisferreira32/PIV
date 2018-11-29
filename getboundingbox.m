24b038c02d69568320d10d34e98dd7b2a4c66672
Cátia Fortunato <catiafortunato35@gmail.com>
2018-11-29 10:26:36 +0000

    add the function that computes bounding box in point cloud

---
+++ getboundingbox.m
@@ -0,0 +1,16 @@
+function [boxpoints]=getboundingbox(pc)
+% Input: 
+% PC: computed points in 3D with size 3xnumpoints
+% Output:
+% boxpoints: vector with the points that form the bounding boz around the
+% object in the point cloud 
+
+xmin=min(pc(:,1));
+xmax=max(pc(:,1));
+ymin=min(pc(:,2));
+ymax=max(pc(:,2));
+zmin=min(pc(:,3));
+zmax=max(pc(:,3));
+boxpoints=[xmin, xmax, ymin, ymax, zmin, zmax];
+
+end
