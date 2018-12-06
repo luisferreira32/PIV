196607d93f3c120cab483f29823d2a591ab35a7a
catiafortunato <catiafortunato35@gmail.com>
2018-12-06 14:19:58 +0000

    Add files via upload

--- get_object_pc.m
+++ get_object_pc.m
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-function [pc]=get_object_pc(pixel_list, imgrgb,imgdepth,bgdepth)
-=======
 function [pc]=get_object_pc(pixel_list, imgrgb,imgdepth, cam_params)
->>>>>>> 8c3033f477b96618428d8943e227698d26273e56
 % Inputs: 
 % pixel_list- list of pixels that belong to the same object
 % imgrgb- rgb image obtained from the reconstruction 
-% imgdepth- depth image corresponding to the rgb image with background
-% subtraction
+% imgdepth- depth image corresponding to the rgb image 
 %
 % Outputs:
 % pc - point cloud obtained from the depth and rgb image
 
-imdiff=abs(imgdepth(:,:,10)-bgdepth)>.25;
-
 rgb_index=zeros(length(pixel_list),3);
-<<<<<<< HEAD
-depth_index=zeros(length(pixel_list,1));
-=======
 depth_index=zeros(length(pixel_list),1);
->>>>>>> 8c3033f477b96618428d8943e227698d26273e56
 
 % Get the pixel values from the rgb image 
 for i=1:length(pixel_list)
@@ -29,11 +18,7 @@ end
 % Get the pixel values from the depth image 
 
 for i=1:length(pixel_list)
-<<<<<<< HEAD
-    depth_index(i)=imdiff(pixel_list(i,2),pixel_list(i,1));
-=======
     depth_index(i)=imgdepth(pixel_list(i,1),pixel_list(i,2));
->>>>>>> 8c3033f477b96618428d8943e227698d26273e56
 end
 
 Z=double(depth_index(:)');
