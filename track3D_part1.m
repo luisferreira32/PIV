53639b398ddc0b3e77ca5ca169baaa004cc945b1
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2018-12-06 14:51:03 +0000

    prints to track program

--- track3D_part1.m
+++ track3D_part1.m
@@ -5,20 +5,20 @@ function [objects] = track3D_part1( imgseq1,   cam_params)
 [film_length, imgsrt, imgsd]=loader(imgseq1, cam_params);
 %[imgsrt, imgsd]=loader(imgseq1, cam_params);
 % film_length = length(imgseq1);
-log_ = "done loading"
+fprintf("done loading");
 
 %% 2. Now box objects moving
 % we extract object boxes and points clouds to further use
 
 [image_objects, image_pcs] = extract_objects(film_length, imgsrt, imgsd, cam_params);
-log_ = "done extracting"
+fprintf("done extracting");
 
 %% 3. Identify which objects are the same, creating a path between boxes
 % connect boxes paths using proximity of boxes (and possible size)
 % then use colour from imgsrt, (lastly use features)
 
 objects = get_path(film_length, image_objects, image_pcs);
-log_ = "done tracking"
+fprintf("done tracking");
 % test it graphically?
 
 end
