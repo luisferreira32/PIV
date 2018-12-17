6772adbc56f53cd74f614aad3adcda15ad78107a
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2018-12-17 23:39:45 +0000

    Update get_RT21.m

--- get_RT21.m
+++ get_RT21.m
@@ -1,4 +1,3 @@
 function [R21, T21] = get_RT21(film_length, imgsrt1, imgsrt2, imgsd1, imgsd2, cam_params)
-	% call this function N times, depending on film_length
-	[R21, T21] = single_RT(rgbd1, rgbd2, imgsd1, imgsd2, cam_params);
+	% use N dependent on film_length, pairs of images, imgsrt - rgb rotated + translated, imgsd - matrix of depth
 end
