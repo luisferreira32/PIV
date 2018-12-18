4611d16b910804b2f7df3c7a12fcbf53df3fa6d4
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2018-12-18 16:02:16 +0000

    update colour cost

--- track3D_part1.m
+++ track3D_part1.m
@@ -369,19 +369,18 @@ function [cost] = cost_colour(pc1, pc2)
     linind2 = find(hsv2(:,2)<.05);
     % the 50000 was random ~16%, img of size 480*640 =~ 300k
     if length(linind1) > 50000 || length(linind2) > 50000
-    	% use the saturation
         h1 = imhist(hsv1(:,2),256);
         h2 = imhist(hsv2(:,2),256);
         %normalize
-        h1=h1/size(hsv1,1)/size(hsv1,2);
-        h2=h2/size(hsv2,1)/size(hsv2,2);
+        h1=h1/sum(h1);
+        h2=h2/sum(h2);
     else
-        % use the hue
+        % maybe nullify the lower saturation hues?
         h1 = imhist(hsv1(:,1),256);
         h2 = imhist(hsv2(:,1),256);
         %normalize
-        h1=h1/size(hsv1,1)/size(hsv1,2);
-        h2=h2/size(hsv2,1)/size(hsv2,2);
+        h1=h1/sum(h1);
+        h2=h2/sum(h2);
     end
 
     % returns a cost between 0 and 1?
