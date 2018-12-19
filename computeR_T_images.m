c4e43a8451da86dea8a792e486bee54cb7354a5b
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2018-12-19 15:22:43 +0000

    "working" read description
    mudei o setup inicial para usar o loader (uma função que dada a estrutura devolver depth matrix + rgbd)
    o erro que fazia dar identidade era em unidades, esqueceram-se de mudar de double a rgbd para uint8 antes de fazer o single (acho que era isso, não sei de mudei mais cenas entre testes)
    não brinquei com o treshold nem iterações.

--- computeR_T_images.m
+++ computeR_T_images.m
@@ -1,60 +1,46 @@
 %% Load data
-load cameraparametersAsus.mat
-%load calib_asus.mat
-d1=dir('/Users/catiafortunato/Documents/MATLAB/piv_project/proj2/fruta3/depth1*.mat');
-d2=dir('/Users/catiafortunato/Documents/MATLAB/piv_project/proj2/fruta3/depth2*.mat');
-dd1=dir('/Users/catiafortunato/Documents/MATLAB/piv_project/proj2/fruta3/rgb_image1*.png');
-dd2=dir('/Users/catiafortunato/Documents/MATLAB/piv_project/proj2/fruta3/rgb_image2*.png');
+clear;
+dir_str = "datasets/bonecos/";
+load("cameraparametersAsus.mat")
+
+% do structs
+% load all the images and mat
+imgjpg1 = strcat(dir_str,'rgb_image1*.png');
+imgmat1 = strcat(dir_str, 'depth1*.mat');
+d1=dir(imgjpg1);
+dd1=dir(imgmat1);
+imgjpg2 = strcat(dir_str,'rgb_image2*.png');
+imgmat2 = strcat(dir_str, 'depth2*.mat');
+d2=dir(imgjpg2);
+dd2=dir(imgmat2);
 %% Images selected
-samples=fix(0.20*length(d1));
+samples=fix(0.10*length(d1));
 img_index=fix(rand(samples,1)*length(d1))+1;
 
 for i=1:length(img_index)
-    img1(:,:,:,i)=imread(dd1(img_index(i)).name);
-end
-for i=1:length(img_index)
-    img2(:,:,:,i)=imread(dd2(img_index(i)).name);
-end
-
-%% Depth image
-
-for i=1:length(img_index)
-    load(d1(img_index(i)).name)
-    dp1(:,:,i)=depth_array;
-end
-for i=1:length(img_index)
-    load(d2(img_index(i)).name)
-    dp2(:,:,i)=depth_array;
+    imgseq1(i).rgb = fullfile(d1(img_index(i)).folder,d1(img_index(i)).name);
+    imgseq1(i).depth = fullfile(dd1(img_index(i)).folder,dd1(img_index(i)).name);
+    imgseq2(i).rgb = fullfile(d2(img_index(i)).folder,d2(img_index(i)).name);
+    imgseq2(i).depth = fullfile(dd2(img_index(i)).folder,dd2(img_index(i)).name);
 end
 
-%% Get intrinsic parameters
+%% Load
+[~, rgbd1, dp1] = loader( imgseq1, cam_params);
+[~, rgbd2, dp2] = loader( imgseq2, cam_params);
 
-K=cam_params.Kdepth;
-
-%% Compute projection coordinates in camera frame
-
-XYZ1=[];
+%% Compute 3d in camera frame
+[v, u]=ind2sub([480 640],(1:480*640));
 for i=1:length(img_index)
-    dp=dp1(:,:,i);
-    xyz1= get_xyzasus(dp(:),[480 640],1:640*480,K,1,0);
-    XYZ1=[XYZ1 xyz1];
-end
-
-XYZ2=[];
-for i=1:length(img_index)
-    dp=dp2(:,:,i);
-    xyz2= get_xyzasus(dp(:),[480 640],1:640*480,K,1,0);
-    XYZ2=[XYZ2 xyz2];
-end
-
-%% Register RGB to depth 
-
-for i=1:length(img_index)
-    rgbd1(:,:,:,i)= get_rgbd(XYZ1(:,i:i+2), img1(:,:,:,i), cam_params.R, cam_params.T, cam_params.Krgb);
-end
-
-for i=1:length(img_index)
-    rgbd2(:,:,:,i)= get_rgbd(XYZ2(:,i:i+2), img2(:,:,:,i), cam_params.R, cam_params.T, cam_params.Krgb);
+    % cam 1
+    Z = dp1(:,:,i);
+    Z = Z(:)';
+    P=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z];
+    xyz1(:,:,i) = P';
+    % cam 2
+    Z = dp2(:,:,i);
+    Z = Z(:)';
+    P=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z];
+    xyz2(:,:,i) = P';
 end
 
 %% Perform SIFT for all images 
@@ -62,31 +48,31 @@ P1=[];
 P2=[];
 
 for i=1:length(img_index)
-    I1=single(rgb2gray(rgbd1(:,:,:,i)));
-    I2=single(rgb2gray(rgbd2(:,:,:,i)));
-    [f1,d1]=vl_sift(I1);
-    [f2,d2]=vl_sift(I2);
-    [matches, scores] = vl_ubcmatch(d1, d2);
+    I1=single(rgb2gray(uint8(rgbd1(:,:,:,i))));
+    I2=single(rgb2gray(uint8(rgbd2(:,:,:,i))));
+    [f1,d1_]=vl_sift(I1);
+    [f2,d2_]=vl_sift(I2);
+    [matches, scores] = vl_ubcmatch(d1_, d2_);
     y1=round(f1(2,matches(1,:)));
     x1=round(f1(1,matches(1,:)));
     y2=round(f2(2,matches(2,:)));
     x2=round(f2(1,matches(2,:)));
-    ind1=sub2ind(size(dp2),y1,x1);
-    ind2=sub2ind(size(dp2),y2,x2);
-    p1=XYZ1(ind1,i:i+2);
-    p2=XYZ2(ind2,i:i+2);
+    ind1=sub2ind(size(dp2(:,:,i)),y1,x1);
+    ind2=sub2ind(size(dp2(:,:,i)),y2,x2);
+    p1=xyz1(ind1,:,i);
+    p2=xyz2(ind2,:,i);
     P1=[P1; p1];
     P2=[P2; p2];
 end
 
 %% Ransac
 n_it=1000; % ??
-threshold=0.005;
+threshold=0.1;
 num_in=[];
 trans=[];
 % Generate random numbers
 n_points=fix(rand(4*n_it,1)*length(P1))+1;
-
+%%
 espera=1;
 
 for i=1:n_it-3
@@ -100,7 +86,7 @@ for i=1:n_it-3
     X2=P2(rand_points,:);
     
     % Compute procrustes
-    [d, Z, transform]=procrustes(X1,X2);
+    [~, ~, transform]=procrustes(X1,X2);
     
     % Compute points from image 2 projected to image 1 using the
     % transformation from procrustes
@@ -108,7 +94,7 @@ for i=1:n_it-3
     xyz21= transform.b * P2 * transform.T + repmat(transform.c(1,:),length(P2),1);
     error=sqrt(sum((P1-xyz21).^2,2)); % Compute the error for all points
     
-    inds(i).in=find(error<0.1);
+    inds(i).in=find(error<threshold);
     
     % Add the number of inliers to the inliers vector
     
@@ -122,9 +108,9 @@ for i=1:n_it-3
     
 end
 %%
-[m, ind]=max(num_in);
+[~, ind]=max(num_in);
 xyz1f=P1(inds(ind).in,:);
 xyz2f=P2(inds(ind).in,:);
-[d, T, transform]=procrustes(xyz1f, xyz2f);
+[~, ~, transform]=procrustes(xyz1f, xyz2f);
 R=transform.T;
 T=transform.c(1,:);
